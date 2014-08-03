f23258988c8e9f6f28cd3dfabb8388347d696e1a
perry2008084 <perry2008084@gmail.com>
2014-08-03 09:02:32 +0800

    添加计算功能

--- BMI/src/com/demo/android/bmi/Bmi.java
+++ BMI/src/com/demo/android/bmi/Bmi.java
@@ -3,6 +3,7 @@ package com.demo.android.bmi;
 import java.text.DecimalFormat;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Bmi extends ActionBarActivity {
 
@@ -20,19 +22,35 @@ public class Bmi extends ActionBarActivity {
 		
 		// listen for button clicks
 		Button button = (Button)findViewById(R.id.submit);
-		button.setOnClickListener(calcBMI);
+		button.setOnClickListener((android.view.View.OnClickListener) calcBMI);
 	}
 	
-	private OnClickListener calcBMI = new OnClickListener () {
-		
-		public void onClick(View v)
-		{
+	private android.view.View.OnClickListener calcBMI = new android.view.View.OnClickListener() {	
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
 			DecimalFormat nf = new DecimalFormat("0.00");
 			EditText fieldheight = (EditText)findViewById(R.id.height);
-			EditText fieldwidth = (EditText)findViewById(R.id.width);
+			EditText fieldweight = (EditText)findViewById(R.id.weight);
+			double height = Double.parseDouble(fieldheight.getText().toString())/100;
+			double weight = Double.parseDouble(fieldweight.getText().toString());
+			double BMI = weight/(height * height);
+			TextView result = (TextView)findViewById(R.id.result);
+			result.setText("Your BMI is "+nf.format(BMI));
 			
+			// give health advice
+			TextView fieldsuggest = (TextView)findViewById(R.id.suggest);
+			if(BMI>25) {
+				fieldsuggest.setText(R.string.advice_heavy);
+			}
+			else if(BMI<20) {
+				fieldsuggest.setText(R.string.advice_light);
+			}
+			else {
+				fieldsuggest.setText(R.string.advice_average);
+			}
 		}
-	}
+	};
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
