2a34802263a2030b40b8c2bb91dbacffa6807f6e
perry2008084 <perry2008084@gmail.com>
2014-08-31 23:43:08 +0800

    使用自定义Adapter方法填充ListView

--- ListViewDemo/app/src/main/java/com/bookcell/listviewdemo/MainActivity.java
+++ ListViewDemo/app/src/main/java/com/bookcell/listviewdemo/MainActivity.java
@@ -10,23 +10,34 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Toast;
+import android.widget.TextView;
+import android.widget.SimpleAdapter;
+
+import com.bookcell.listviewdemo.MyAdapter;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
     String [] myStringArray = {"笑傲江湖", "连城诀", "天龙八部"};
+
+    private TextView describe;
+    private ListView listView;
+    private String[] userName = {"User 1","User 2","User 3","User 4"};
+    private String[] userDescribe = {"China","Japan","Norge","Sweden"};
+    private int[] userIcon = { R.drawable.icon_1, R.drawable.icon_2,R.drawable.icon_3, R.drawable.icon_4 };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, myStringArray);
-
-        ListView listView = (ListView) findViewById(R.id.listview);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(mMessageClickedHandler);
+        //initAll_arrayAdapter();
+        //initAll_simpleAdapter();
+        initAll_myAdapter();
     }
 
 
@@ -57,4 +68,44 @@ public class MainActivity extends Activity {
         }
     };
 
+    public void initAll_arrayAdapter(){
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, myStringArray);
+
+        ListView listView = (ListView) findViewById(R.id.listview);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(mMessageClickedHandler);
+    }
+
+    public void initAll_simpleAdapter(){
+        describe = (TextView)findViewById(R.id.list_view_describe);
+        listView = (ListView)findViewById(R.id.listview);
+        describe.setText("使用了SimpleAdapter填充的ListView");
+        List<Map<String, Object>> dateLists = new ArrayList<Map<String, Object>>();
+        for(int i = 0; i < userName.length ; i++)
+        { Map<String, Object> dateItem = new HashMap<String, Object>();
+            dateItem.put("icon",userIcon[i]);
+            dateItem.put("name",userName[i]);
+            dateItem.put("describe",userDescribe[i]);
+            dateLists.add(dateItem);
+        }
+        String[] keysOfMap = {"icon","name","describe"};
+        int [] keyOfLayout = { R.id.item_simple_adapter_icon, R.id.item_simple_adapter_name, R.id.item_simple_adapter_describe};
+        SimpleAdapter simpleAdapter = new SimpleAdapter (this,dateLists,R.layout.simple_list_item_1,keysOfMap,keyOfLayout);
+        listView.setAdapter(simpleAdapter);
+    }
+
+    public void initAll_myAdapter() {
+        describe = (TextView)findViewById(R.id.list_view_describe);
+        listView = (ListView)findViewById(R.id.listview);
+        describe.setText("user myAdapter fill ListView");
+        ArrayList<com.bookcell.listviewdemo.BookInfo> bookInfoArrayList = new ArrayList<com.bookcell.listviewdemo.BookInfo>();
+        bookInfoArrayList.add(new com.bookcell.listviewdemo.BookInfo("阿潘", "China", R.drawable.icon_1));
+        bookInfoArrayList.add(new com.bookcell.listviewdemo.BookInfo("鱼仔", "China", R.drawable.icon_2));
+        bookInfoArrayList.add(new com.bookcell.listviewdemo.BookInfo("Totoro", "Japan", R.drawable.icon_3));
+        MyAdapter myAdapter = new MyAdapter(this,bookInfoArrayList);
+        listView.setAdapter(myAdapter);
+
+    }
 }
