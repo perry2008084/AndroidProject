6f1ea6491c71736c1e64c71a520401355b1d5a6a
perry2008084 <perry2008084@gmail.com>
2014-10-13 22:51:32 +0800

    修改从网络获取图书数据

--- BookCell/app/src/main/java/com/bookcell/NewBookDao.java
+++ BookCell/app/src/main/java/com/bookcell/NewBookDao.java
@@ -3,6 +3,7 @@ package com.bookcell;
 /**
  * Created by perry on 2014/10/12.
  */
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -15,9 +16,12 @@ import net.htmlparser.jericho.Source;
 public class NewBookDao {
     public static List<BookInfo> getAllNewBooks() throws Exception{
         List<BookInfo> list = new ArrayList<BookInfo>();
-        URL url = new URL("http://book.douban.com/latest");
+        //URL url = new URL("http://book.douban.com/latest");
+        URL url = new URL("http://book.douban.com/subject/1231576/");
+
         URLConnection conn = url.openConnection();
         Source source = new Source(conn);
+        /*
         List<Element> liList = source.getAllElements("li");
         for(Element element:liList){
             List<Element>  childrenList = element.getChildElements();
@@ -34,6 +38,34 @@ public class NewBookDao {
                 list.add(newBook);
             }
         }
+        */
+
+        BookInfo newBook = new BookInfo();
+        List<Element> aList = source.getAllElements("a");
+        for(Element element:aList)
+        {
+            //if(element.getClass().getName() == "nbg")
+            if("nbg".equals(element.getAttributeValue("class")))
+            {
+                String name = element.getAttributeValue("title");
+                newBook.setName(name);
+
+                String path = element.getAttributeValue("href");
+                newBook.setBookPicturePath(path);
+            }
+        }
+
+        List<Element> divList = source.getAllElements("div");
+        for(Element element:divList)
+        {
+            if("intro".equals(element.getAttributeValue("class")))
+            {
+                String message = element.getAllElements().get(0).getTextExtractor().toString();
+                newBook.setDescribe(message);
+            }
+        }
+        list.add(newBook);
+
         return list;
     }
 }
